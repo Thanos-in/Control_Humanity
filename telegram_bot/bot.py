a2d748547ac2cee0252365d6a29070a55ad02ad9
Thanos-in <sayyodbee2006@gmail.com>
2025-06-22 00:47:25 +0500

    Changes tg-bot

--- telegram_bot/bot.py
+++ telegram_bot/bot.py
@@ -1,3 +1,5 @@
+#===========================================================>>>>
+
 from telegram import (
     Update,
     ReplyKeyboardMarkup,
@@ -20,11 +22,11 @@ TASKS_URL = 'http://localhost:8000/api/telegram-tasks/'
 
 # /start — telefon raqamini so'raydi
 async def start_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    contact_btn = KeyboardButton(text="📱 Telefon raqamini yuborish", request_contact=True)
+    contact_btn = KeyboardButton(text="📱 Отправить номер телефона", request_contact=True)
     kb = [[contact_btn]]
     reply_markup = ReplyKeyboardMarkup(kb, resize_keyboard=True, one_time_keyboard=True)
     await update.message.reply_text(
-        "Assalomu alaykum! Iltimos, telefon raqamingizni yuboring:",
+        "Здравствуйте! Пожалуйста, отправьте свой номер телефона:",
         reply_markup=reply_markup
     )
 
@@ -35,14 +37,14 @@ async def contact_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     # Faqat o'z kontaktingiz bo'lsa
     if contact.user_id != user.id:
-        await update.message.reply_text("❌ Iltimos, o'zingizning raqamingizni yuboring.")
+        await update.message.reply_text("❌ Пожалуйста, отправьте свой номер.")
         return
 
     phone = contact.phone_number
     normalized_phone = f"+{phone.lstrip('+')}"
     telegram_id = user.id
-    print(f"📞 Yuborilgan raqam: {normalized_phone}")
-    print(f"👤 Telegram ID: {telegram_id}")
+    # print(f"Телефон: {normalized_phone}")
+    # print(f"Telegram ID: {telegram_id}")
     try:
         resp = requests.post(BIND_URL, json={
             "phone": normalized_phone,
@@ -50,7 +52,7 @@ async def contact_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
         }, timeout=5)
         if resp.status_code == 200:
             await update.message.reply_text(
-                "✅ Telefon raqamingiz muvaffaqiyatli bog‘landi!\nEndi /mytasks komandasini yuboring.",
+                "✅ Ваш номер телефона успешно связан!\nТеперь команду /mytasks отправив, вы сможете увидеть задачи, закреплённые за вами.",
                 reply_markup=ReplyKeyboardRemove()
             )
         else:
@@ -109,55 +111,3 @@ def main():
 if __name__ == "__main__":
     main()
 
-
-#===========================================================>>>>
-
-
-# from telegram import Update
-# from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
-# import requests
-# from datetime import datetime   
-
-# BOT_TOKEN = '8040016996:AAGRy-acyXz_Y3UwdJINC4oxsphE39BgeyE'
-# BACKEND_URL = 'http://localhost:8000/api/telegram-tasks/'
-
-# async def task_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
-#     chat_id = update.effective_chat.id
-
-#     try:
-#         response = requests.get(BACKEND_URL, params={'telegram_id': chat_id})
-#         data = response.json()
-
-#         if response.status_code != 200 or not data:
-#             await context.bot.send_message(chat_id=chat_id, text="✅ У вас пока нет задач.")
-#             return
-
-#         msg = "📋 *Задачи, связанные с вами:*\n\n"
-#         msg += f"*{'№':<3} {'Наименование':<30} Статус        Сроки*\n"
-
-#         for i, task in enumerate(data, 1):
-#             title = task['title'][:10] + '...' if len(task['title']) > 30 else task['title']
-#             status = task['status']['name']
-
-#             def format_date(field):
-#                 raw = task.get(field)
-#                 try:
-#                     return datetime.fromisoformat(raw).strftime('%d.%m.%Y') if raw else '—'
-#                 except:
-#                     return '—'
-
-#             created = format_date('created_at')
-#             updated = format_date('updated_at')
-#             due = format_date('due_date')
-
-#             period_str = f"{created} → {updated} → {due}"
-#             msg += f"{i:<3} {title:<30} {status:<12} {period_str}\n"
-
-#         await context.bot.send_message(chat_id=chat_id, text=msg, parse_mode='Markdown')
-
-#     except Exception as e:
-#         await context.bot.send_message(chat_id=chat_id, text=f"Ошибка!: {str(e)}")
-
-# app = ApplicationBuilder().token(BOT_TOKEN).build()
-# app.add_handler(CommandHandler("mytasks", task_handler))
-# app.run_polling()
